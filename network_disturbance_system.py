4437026a7f9eec8a7c641d3d5959bf8915165664
Arthur <arthur.zachow@gmail.com>
2017-10-15 20:07:31 -0200

    python2 changes

--- network_disturbance_system.py
+++ network_disturbance_system.py
@@ -17,33 +17,6 @@ import igraph as iG
 import MSA.successive_averages as MSA
 import system_optimal_solver.so_solver as SO
 
-def export_to_igraph_file(edge_list, with_cost=False): #Don't think we'll use this one.
-    '''
-    This function exports a network to an edge list file to be read by the igraph module.
-    In:
-        edge_list:List = List of Edge class objects.
-        with_cost:Boolean = If it is necessary to print into the file the cost of the edges.
-    '''
-    #Define folder to store the graphs generated
-    path = './network_graphs/'
-
-    #Creates the folder if it doesn`t exist
-    if os.path.isdir(path) is False:
-        os.makedirs(path)
-
-    fh = open(path+NETWORK_NAME, 'w')
-
-    #Iterates through the list of edges printing the start and end in the file.
-    #It uses the format of edge list for the file input in igraph.
-    for edge in edge_list:
-        print('{} {}'.format(edge.start, edge.end), end='', file=fh)
-        if with_cost: # If want to print the weight of the edges in the graph too.
-            print(' {}'.format(edge.cost), file=fh)
-        else:
-            print('', file=fh)
-
-    fh.close()
-
 def export_to_igraph(node_list, edge_list, with_cost=False):
     """
     Exports the network that's described by node_list and edge_list to an iGraph graph.
