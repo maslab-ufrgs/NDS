b3a258dfa38218c4b2763bfe7b71a5e51543f15a
Arthur <arthur.zachow@gmail.com>
2017-10-17 10:25:40 -0200

    complementary edges change and documentation correction

--- network_disturbance_system.py
+++ network_disturbance_system.py
@@ -44,7 +44,7 @@ def export_to_igraph(node_list, edge_list, with_cost=False):
 
     return graph
 
-def assignment(net_file='', episodes=400, edge_list=None, node_list=None, od_matrix=None):
+def assignment(net_file='', iterations=400, edge_list=None, node_list=None, od_matrix=None):
     """
     Calls the MSA and System Optimal Solver to compute UE (User Equilibrium) and SO (System Optimal)
     and PoA (Price of Anarchy), respectively.
@@ -52,7 +52,7 @@ def assignment(net_file='', episodes=400, edge_list=None, node_list=None, od_mat
     **Needs either a network file path or (edge_list and node_list and od_matrix) to work.
     In:
         net_file:String = The absolute path to the network file.
-        episodes:Integer = Number of episodes for the MSA run.
+        iterations:Integer = Number of iterations for the MSA run.
         edge_list:List = Node objects from the MSA module.
         node_list:List = Edge objects from the MSA module.
         od_matrix:Dictionary = Represents the OD pairs and their demands.
@@ -65,11 +65,11 @@ def assignment(net_file='', episodes=400, edge_list=None, node_list=None, od_mat
         PoA:Float = Price of Anarchy.
     """
     if edge_list and node_list and od_matrix:
-        nodes, edges, od_matrix, ue = MSA.run(episodes, edge_list=edge_list, node_list=node_list,
+        nodes, edges, od_matrix, ue = MSA.run(iterations, edge_list=edge_list, node_list=node_list,
                                               od_matrix=od_matrix, output=False)
     else:
         #Calls MSA for network and UE
-        nodes, edges, od_matrix, ue = MSA.run(episodes, net_file=net_file, output=False)
+        nodes, edges, od_matrix, ue = MSA.run(iterations, net_file=net_file, output=False)
 
     #Calls System Optimal Solver for SO
     so = SO.SOSolver(nodes, edges, od_matrix)
@@ -81,16 +81,17 @@ def assignment(net_file='', episodes=400, edge_list=None, node_list=None, od_mat
 
     return nodes, edges, od_matrix, ue, sop, PoA
 
-def change_edges(node_list, edge_list, od_matrix):
+def change_edges(node_list, edge_list, od_matrix, complementary_edges):
     """
     Makes random changes in a graph's edges (defined as traffic network).
     In:
         node_list:List = List of Node objects from the MSA module.
         edge_list:List = List of Edge objects from the MSA module.
+        od_matrix:Dictionary = Dictionary of the OD pairs with the associated demand.
+        complementary_edges:Boolean = If it needs to change the complementary edge too.
     Out:
         edge_ln:List = New list of Edge objects representing the new graph.
         changed_edge:String = String representing the edge that was changed.
-    #Maybe it does't need the new list.
     #Probably a function that can get stuck in a loop, because it can have possibly 0 connected
     ##graph that can be generated from it
     """
@@ -106,30 +107,32 @@ def change_edges(node_list, edge_list, od_matrix):
         #Checks if the 2 nodes are equal or they are an OD pair
         if not (node1 == node2 or (str(node1.name + '|' + node2.name) in od_matrix)):
             #Changes the edge start and end
-            """
-            Needs to the for actually two edges, because an undirected graph is represented as
-                having two directed edges, let's call it complementary edge. The graph isn't
-                necessarily an undirected graph.
-            """
-            #Gets this complementary edge
-            has_comp_edge = False
-            for edge in edge_ln:
-                if edge.end == rnd_edge.start and edge.start == rnd_edge.end:
-                    comp_edge = edge
-                    has_comp_edge = True
-                    break
+            if complementary_edges:
+                """
+                Needs to the for actually two edges, because an undirected graph is represented as
+                    having two directed edges, let's call it complementary edge. The graph isn't
+                    necessarily an undirected graph.
+                """
+                #Gets this complementary edge
+                has_comp_edge = False
+                for edge in edge_ln:
+                    if edge.end == rnd_edge.start and edge.start == rnd_edge.end:
+                        comp_edge = edge
+                        has_comp_edge = True
+                        break
             #Changes the random edge
             rnd_edge.start = node1.name
             rnd_edge.end = node2.name
 
-            #If we change the name of the edge, we`ll receive warnings from Docplex because of
-            ##duplicate names sometimes
-            #rnd_edge.name = "{0}-{1}".format(node1.name, node2.name)
-            #Changes the complementary edges, if it has one
-            if has_comp_edge:
-                comp_edge.start = node2.name
-                comp_edge.end = node1.name
-                #comp_edge.name = "{0}-{1}".format(node2.name, node1.name)
+            if complementary_edges:
+                #If we change the name of the edge, we`ll receive warnings from Docplex because of
+                ##duplicate names sometimes
+                #rnd_edge.name = "{0}-{1}".format(node1.name, node2.name)
+                #Changes the complementary edges, if it has one
+                if has_comp_edge:
+                    comp_edge.start = node2.name
+                    comp_edge.end = node1.name
+                    #comp_edge.name = "{0}-{1}".format(node2.name, node1.name)
 
             #Checks if the graph is connected, if it is, then the function is over and a new graph
             ##has been found
@@ -139,12 +142,13 @@ def change_edges(node_list, edge_list, od_matrix):
 
     return edge_ln, changed_edge
 
-def print_results(net_name, changed_edges_list, episodes, UE, SO, PoA, graph):
+def print_results(net_name, changed_edges_list, iterations, UE, SO, PoA, graph):
     """
     Prints the results in a table.
     In:
         net_name:String = Name of the network.
         changed_edges_list:List = List of the changes made in the edges.
+        iterations:Integer = Number of iterations in the MSA.
         UE:Float = User equilibrium.
         SO:Float = System optimal.
         PoA:Float = Price of Anarchy.
@@ -158,11 +162,11 @@ def print_results(net_name, changed_edges_list, episodes, UE, SO, PoA, graph):
     #Average value of the edge betweenness of the graph
     avg_betweenness = sum(graph_betweenness)/len(graph.es)
     #Prints some kind of table
-    print('#Network name = {0}\t# of episodes = {1}'.format(net_name, episodes))
+    print('#Network name = {0}\t# of iterations = {1}'.format(net_name, iterations))
     print('#User Equilibrium\tSystem Optimal\tPrice of Anarchy\tEdge Betweenness\tEach edge betweenness')
     print('{0}\t{1}\t{2}\t{3}\t{4}'.format(UE, SO, PoA, avg_betweenness,
-                                           [((graph.vs[a.tuple[0]]['name'] + '-' + graph.vs[a.tuple[1]]['name']), b) for a, b in
-                                           zip(graph.es, graph_betweenness)]))
+                                           sorted([((graph.vs[a.tuple[0]]['name'] + '-' + graph.vs[a.tuple[1]]['name']), b) for a, b in
+                                           zip(graph.es, graph_betweenness)])))
 
 def get_network_name(net_name, changed_edges_list):
     """
@@ -184,8 +188,10 @@ def main():
     Upper level function. It calls the other functions in an orderly way.
     Parser inputs:
         file:String = The absolute path to the network file.
-        episodes:Integer = Number of episodes in the MSA run.
+        iterations:Integer = Number of iterations in the MSA run.
         changes:Integer = Number of edges to change in the algorithm.
+        k:Integer = Number of routes for the KSP algorithm.
+        complementary_edges:Boolean = If it is to change only one direction of the edge.
     """
     #Parser things for the parameters
     prs = ArgP.ArgumentParser(formatter_class=ArgP.ArgumentDefaultsHelpFormatter,
@@ -194,12 +200,14 @@ def main():
                                               a traffic network (graph) by removing 1 random edge
                                               and inserting another with the same attributes but
                                               with different start and end nodes.\n
-                                              V1.5
+                                              V1.6
                                               """)
     prs.add_argument("-f", dest="file", required=True, help="The network file.\n")
-    prs.add_argument("-e", "--episodes", type=int, default=1000, help="Number of episodes.\n")
+    prs.add_argument("-i", "--iterations", type=int, default=1000, help="Number of iterations (MSA).\n")
     prs.add_argument("-c", "--changes", type=int, default=1, help="Number of changes in the network.\n")
     prs.add_argument("-k", type=int, default=8, help="Number of routes for KSP algorithm.\n")
+    prs.add_argument("-ce", "--complementary_edges", action="store_true", default=False,
+                     help="If it is to change only one direction of the edge.\n")
     args = prs.parse_args()
 
     #Network name
@@ -213,36 +221,35 @@ def main():
 
     #Start of the algorithm
     #Gets the original nodes and edges
-    nodes, edges, od_matrix, ue, so, PoA = assignment(net_file=args.file, episodes=args.episodes)
+    nodes, edges, od_matrix, ue, so, PoA = assignment(net_file=args.file, iterations=args.iterations)
     #Coupling call
     Coup.calculate_coupling(args.file, None, None, args.k)
     #Gets the original graph
     graph = export_to_igraph(nodes, edges, with_cost=True)
 
-    #Needs to adjust the weights in the betweenness
-    #print_results(net_name, changed_edges, args.episodes, ue, so, PoA,
-    #              sum(graph_o.edge_betweenness(weights='weight'))/len(graph_o.es))
-    print_results(net_name, changed_edges, args.episodes, ue, so, PoA, graph)
+    #Prints the results on-screen
+    print_results(net_name, changed_edges, args.iterations, ue, so, PoA, graph)
 
     #While loop for changing the edges of the graph
     while changes < args.changes:
         #Modified edges of the graph
-        edges, changed_edge = change_edges(nodes, edges, od_matrix)
+        edges, changed_edge = change_edges(nodes, edges, od_matrix, args.complementary_edges)
+        #Append to the list of changes
         changed_edges.append(changed_edge)
         #Number of changes made in the edges
         changes += 1
 
         #Can ignore the output of edges, nodes and od_matrix as they don't change
-        _, _, _, ue, so, PoA = assignment(episodes=args.episodes, edge_list=edges,
+        _, _, _, ue, so, PoA = assignment(iterations=args.iterations, edge_list=edges,
                                           node_list=nodes, od_matrix=od_matrix)
         #Coupling call
         Coup.calculate_coupling(get_network_name(net_name, changed_edges), None, None, args.k,
                                 edge_list=edges, node_list=nodes, od_matrix=od_matrix)
         #Gets the modified graph
         graph = export_to_igraph(nodes, edges, with_cost=True)
-        #print_results(net_name, changed_edges, args.episodes, ue, so, PoA,
-        #              sum(graph_m.edge_betweenness(weights='weight'))/len(graph_m.es))
-        print_results(net_name, changed_edges, args.episodes, ue, so, PoA, graph)
+
+        #Prints the results on-screen
+        print_results(net_name, changed_edges, args.iterations, ue, so, PoA, graph)
 
 
 if __name__ == '__main__':
